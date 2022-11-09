783d29f6b7e2a18a76cf04fbe17a144819851b8f
rafay99-epic <99marafay@gmail.com>
2022-11-09 12:50:09 +0500

    Updating Files in Learning  Branch

--- git.ps1
+++ git.ps1
@@ -1,17 +1,34 @@
-Write-Host("Working on Machine Learning");
+Write-Host("
+-----------------------------
+Working on Machine Learning
+-----------------------------
+");
 # Storing the console output into a Variable.
 $OutputVariable = (git branch --show-current) | Out-String
-# Displaying the Output from the variable
-# Get-Variable OutputVariable
 
-# Checking the Branch and then commiting on the bases of branch
+# Checking the branch using git command
 if( $OutputVariable -eq "main" ) {
-    Write-Host("On Main Branch");
+    Write-Host("
+    -----------------------------
+           On Main Branch
+    -----------------------------");
+    
+    Write-Host("
+    -----------------------------
+         Git Status: Files
+    -----------------------------");
     # Checking the Current Status
-    git status
+    $StatusVariable = (git status) | Out-String
+    Write-Host("
+    -----------------------------
+        Modified Files
+    -----------------------------
+    ");
+    Write-Output $StatusVariable
+    #git status
     # Getting from the main remote repo
     git pull 
-    
+
 } else 
 {
     Write-Host("Not on master, it's the Learning Branch");
@@ -21,10 +38,3 @@ if( $OutputVariable -eq "main" ) {
     git commit -m 'Updating Files in Learning  Branch'
     git push
 }
-# else {
-#     Write-Host("No Branch was Detected");
-#     Write-Host("Please try Again") 
-# }
-
-
-
