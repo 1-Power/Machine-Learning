208f23209fd646a93d15005ea3f60ab15ea7cacb
Abdul Rafay <99marafay@gmail.com>
2022-11-18 16:05:58 +0500

    Machine Learning Model

--- git.sh
+++ git.sh
@@ -8,18 +8,6 @@ output=$(git branch --show-current)
 
 # echo "Current Branch: $output"
 
-# campare string with if statement
-# if [ "$output" = "Machine-Learning" ]; then
-#     echo "You are on the Machine Learning Branch"
-#     git add *
-#     git commit -m "Machine Learning Model"
-#     git push
-# else
-#     echo "You are not on the Machine Learning Branch"
-#     git checkout main
-#     git pull
-# fi
-
 if [ "$output" = "main" ];then 
     echo "
 ------------------------------------------------
